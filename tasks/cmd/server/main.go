bc70952840cda8298ed4516df4b5f86fb2f06b37
literaen <literaen@gmail.com>
2025-04-24 11:38:39 +0300

    Удаляет users, tasks

--- tasks/cmd/server/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/literaen/simple_project/tasks/internal/web"
-
-func main() {
-	web := web.NewWeb()
-	web.Init()
-	web.Run()
-}
--- users/cmd/server/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/literaen/simple_project/users/internal/web"
-
-func main() {
-	web := web.NewWeb()
-	web.Init()
-	web.Run()
-}
